77bf89a98325d926524403e4ce4ecf63b0196a12
Yuhe Liu <junetheriver@gmail.com>
2023-08-08 09:53:32 +0000

    add opsqa, waiting for implementation

---
+++ configs/datasets/opsqa/opsqa_gen.py
@@ -0,0 +1,43 @@
+from opencompass.openicl.icl_prompt_template import PromptTemplate
+from opencompass.openicl.icl_retriever import FixKRetriever
+from opencompass.openicl.icl_inferencer import GenInferencer
+from opencompass.openicl.icl_evaluator import AccEvaluator
+from opencompass.utils.text_postprocessors import first_capital_postprocess
+from opencompass.datasets import OpsQADataset
+
+opsqa_reader_cfg = dict(
+    input_columns=['input','A','B','C','D'],
+    output_column='target')
+
+opsqa_prompt_template = dict(
+    type='PromptTemplate',
+    template=None,
+    ice_token='</E>')
+
+opsqa_infer_cfg = dict(
+    ice_template=dict(
+        type=PromptTemplate,
+        template=dict(round=[
+            dict(
+                role='HUMAN',
+                prompt='{input}\nA. {A}\nB. {B}\nC. {C}\nD. {D}\nAnswer: '
+            ),
+            dict(role='BOT', prompt='{target}\n')
+        ])),
+    prompt_template=opsqa_prompt_template,
+    retriever=dict(type=FixKRetriever),
+    inferencer=dict(type=GenInferencer, fix_id_list=[0, 1, 2, 3, 4]))
+
+opsqa_eval_cfg = dict(
+    evaluator=dict(type=AccEvaluator),
+    pred_postprocessor=dict(type=first_capital_postprocess))
+
+triviaqa_datasets = [
+    dict(
+        type=TriviaQADataset,
+        abbr='triviaqa',
+        path='./data/triviaqa/',
+        reader_cfg=triviaqa_reader_cfg,
+        infer_cfg=triviaqa_infer_cfg,
+        eval_cfg=triviaqa_eval_cfg)
+]
---
+++ opencompass/datasets/opsqa.py
@@ -0,0 +1,30 @@
+import csv
+import os.path as osp
+
+from datasets import Dataset, DatasetDict
+
+from opencompass.registry import LOAD_DATASET
+
+from .base import BaseDataset
+
+
+@LOAD_DATASET.register_module()
+class OpsQADataset(BaseDataset):
+
+    @staticmethod
+    def load(path: str):
+        raw_data = []
+        filename = osp.join(path, 'demo_csv.csv')
+        with open(filename, encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                assert len(row) == 6
+                raw_data.append({
+                    'input': row[0],
+                    'A': row[1],
+                    'B': row[2],
+                    'C': row[3],
+                    'D': row[4],
+                    'target': row[5],
+                })
+        return Dataset.from_dict(raw_data)
