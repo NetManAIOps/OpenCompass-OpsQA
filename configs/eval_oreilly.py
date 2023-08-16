adb1c8de0553b651b38f7885087efacbe3b04a0a
xu-ll18 <2621922727@qq.com>
2023-08-16 20:19:15 +0800

    chinese llama2

--- configs/eval_oreilly.py
+++ configs/eval_oreilly.py
@@ -1,5 +1,5 @@
 from mmengine.config import read_base
-from opencompass.partitioners import SizePartitioner
+from opencompass.partitioners import SizePartitioner, NaivePartitioner
 from opencompass.runners import LocalRunner
 from opencompass.tasks import OpenICLInferTask, OpenICLEvalTask
 
@@ -8,40 +8,50 @@ with read_base():
     from .datasets.opsqa.opsqa_demo_choice_gen import opsqa_datasets as choice_gen
     from .datasets.opsqa.opsqa_demo_choice_ppl import opsqa_datasets as choice_ppl
     # from .datasets.opsqa.opsqa_demo_compr import opsqa_datasets
-    from .datasets.oreilly.oreilly_gen import oreilly_datasets as oreilly
+    from .datasets.oreilly.oreilly_gen_1 import oreilly_datasets as oreilly
+    from .datasets.oreilly.oreilly_sc import oreilly_datasets as oreilly_sc
     from .local_models.chatglm_6b import models as chatglm_6b
     from .local_models.chatglm2_6b import models as chatglm2_6b
     from .local_models.baichuan_7b import models as baichuan_7b
+    from .local_models.chinese_llama_2_13b import models as chinese_llama_2_13b
+    from .local_models.chinese_alpaca_2_13b import models as chinese_alpaca_2_13b
     from .models.gpt_3dot5_turbo_peiqi import models as peiqi
 
 datasets = [
-    *oreilly[:1] 
+    *oreilly,
+    # *oreilly_sc,
     # *summary, 
     # *choice_gen, 
     # *choice_ppl
-    ]
 
-from opencompass.models import HuggingFaceCausalLM
-
-
-
+]
 models = [ 
-            *peiqi,
-            # *llama2_7b, 
-            # *baichuan_7b,
-            # *chatglm_6b,
-            # *chatglm2_6b
-        ]
+    *chinese_llama_2_13b,
+    # *chinese_alpaca_2_13b,
+    # *llama2_7b, 
+    # *baichuan_7b,
+    # *chatglm_6b,
+    # *chatglm2_6b
+]
 
-# infer = dict(
-#     partitioner=dict(
-#         type=SizePartitioner,
-#         max_task_size = 100,
-#         gen_task_coef = 1,
-#     ),
-#     runner=dict(
-#         type=LocalRunner,
-#         max_num_workers=4,
-#         task=dict(type=OpenICLInferTask),
-#     ),
-# )
+infer = dict(
+    partitioner=dict(
+        type=SizePartitioner,
+        max_task_size = 20000,
+        gen_task_coef = 1,
+    ),
+    runner=dict(
+        type=LocalRunner,
+        max_num_workers=4,
+        max_workers_per_gpu=1,
+        task=dict(type=OpenICLInferTask),
+    ),
+)
+
+eval = dict(
+    partitioner=dict(type=NaivePartitioner),
+    runner=dict(
+        type=LocalRunner,
+        max_num_workers=32,
+        task=dict(type=OpenICLEvalTask)),
+)
---
+++ configs/local_models/chinese_alpaca_2_13b.py
@@ -0,0 +1,24 @@
+from opencompass.models import HuggingFaceCausalLM
+
+_meta_template = dict(
+    round=[
+        dict(role="HUMAN", api_role="HUMAN"),
+        dict(role="BOT", api_role="BOT", generate=True),
+    ],
+)
+
+models = [
+    dict(
+        abbr="chinese-alpaca-2-13b",
+        type=HuggingFaceCausalLM, 
+        path="/mnt/mfs/opsgpt/models/chinese-llama-2/chinese-alpaca-2-13b/",
+        tokenizer_path="/mnt/mfs/opsgpt/models/chinese-llama-2/chinese-alpaca-2-13b/",
+        max_out_len=100,
+        max_seq_len=2048,
+        batch_size=16,
+        meta_template=_meta_template,
+        model_kwargs=dict(device_map='auto', trust_remote_code=True),
+        batch_padding=False,
+        run_cfg=dict(num_gpus=2, num_procs=1),
+    ),
+]
