e0161d83fc65356b4e86c5cde828e3b3a09aeba7
Yuhe Liu <junetheriver@gmail.com>
2023-08-21 01:55:31 +0000

    add xll archive codes and 2 tool script: analyze_common_failcases and extract_failcases

--- tools/extract_failcases.py
+++ tools/extract_failcases.py
@@ -3,7 +3,7 @@ import os  # noqa
 import re  # noqa
 from os import path as osp  # noqa
 
-import pandas as pd
+import pandas as pd  # noqa
 
 from opencompass.datasets import oreilly_choice_postprocess as extract_answer
 
@@ -45,4 +45,19 @@ def extract_fail_cases(folder):
 
 if __name__ == '__main__':
     workdir = '/mnt/mfs/opsgpt/opencompass/outputs/'
-    extract_fail_cases(workdir + 'archive/internxverse-oreilly-sample')
+    # extract_fail_cases(workdir + 'archive/internxverse-oreilly-sample')
+    # extract_fail_cases(workdir + 'archive/baichuan-oreilly-sample')
+    # extract_fail_cases(workdir + 'archive/glmllama-oreilly-sample')
+    # extract_fail_cases(workdir + 'archive/gpt-oreilly-sample')
+    # extract_fail_cases(workdir + 'archive/llama2chat13b-oreilly-full')
+    # extract_fail_cases(workdir + 'archive/gpt3.5-oreilly-full')
+    workdir = '/mnt/mfs/opsgpt/opencompass/archive/v-xll22/outputs/'
+    # extract_fail_cases(workdir + 'few_shot/chatglm/20230818_021329')
+    # extract_fail_cases(workdir + 'few_shot/chatgpt/20230818_001957')
+    # extract_fail_cases(workdir + 'few_shot/internlm/20230817_230433')
+    extract_fail_cases(workdir + 'few_shot/qwen/20230818_023645')
+    # extract_fail_cases(workdir + 'sc/chatglm-6b/20230816_181124')
+    # extract_fail_cases(workdir + 'zero_shot/chatgpt/20230818_023958')
+    # extract_fail_cases(workdir + 'zero_shot/chinese-alpaca-2/20230817_013153')  # noqa
+    # extract_fail_cases(workdir + 'zero_shot/chineses-llama-2-13b/20230816_195746')  # noqa
+    # extract_fail_cases(workdir + 'zero_shot/qwen/20230817_163308')
