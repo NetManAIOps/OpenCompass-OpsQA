4838e9f67b6b0e58300ecf6dc16bdafcb5a81426
River <junetheriver@gmail.com>
2023-09-08 14:55:31 +0800

    add llama2_chinese_13b_chat

---
+++ configs/local_models/llama2_chinese_13b_chat.py
@@ -0,0 +1,24 @@
+from opencompass.models import HuggingFaceCausalLM
+
+_meta_template = dict(
+    round=[
+        dict(role="HUMAN", api_role="HUMAN"),
+        dict(role="BOT", api_role="BOT", generate=True),
+    ],
+)
+
+models = [
+    dict(
+        abbr="Llama2-Chinese-13b-Chat",
+        type=HuggingFaceCausalLM, 
+        path="/gpudata/home/cbh/opsgpt/models/chinese-llama-2/Llama2-Chinese-13b-Chat/",
+        tokenizer_path="/gpudata/home/cbh/opsgpt/models/chinese-llama-2/Llama2-Chinese-13b-Chat/",
+        max_out_len=1024,
+        max_seq_len=2048,
+        batch_size=16,
+        meta_template=_meta_template,
+        model_kwargs=dict(device_map='auto', trust_remote_code=True),
+        batch_padding=False,
+        run_cfg=dict(num_gpus=4, num_procs=1),
+    ),
+]
--- configs/network_fewshot.py
+++ configs/network_fewshot.py
@@ -12,42 +12,45 @@ with read_base():
     from .local_models.chatglm2_6b import models as chatglm2_6b
     from .local_models.qwen_chat_7b import models as qwen_chat_7b
     from .local_models.baichuan_13b_chat import models as baichuan_13b_chat
+    from .local_models.baichuan2_13b_chat import models as baichuan2_13b_chat
     from .local_models.internlm_chat_7b import models as internlm_chat_7b
     from .local_models.llama2_7b_chat import models as llama2_chat_7b
     from .local_models.llama2_13b_chat import models as llama2_chat_13b
     from .local_models.chinese_llama_2_13b import models as chinese_llama_2_13b
     from .local_models.chinese_alpaca_2_13b import models as chinese_alpaca_2_13b
+    from .local_models.llama2_chinese_13b_chat import models as llama2_chinese_13b_chat
     from .local_models.xverse_13b import models as xverse_13b
     from .models.gpt_4_peiqi import models as gpt_4
     from .models.gpt_3dot5_turbo_peiqi import models as chatgpt
 
 datasets = [
-    # *fewshot_naiive, # ok
-    *fewshot_sc, 
-    *fewshot_sc_cot,
+    *fewshot_naiive, # ok
+    # *fewshot_sc, 
+    # *fewshot_sc_cot,
 ]
 
 models = [ 
-    *chatglm2_6b,
-    *qwen_chat_7b,
-    *llama2_chat_7b,
-    *internlm_chat_7b,
-    *baichuan_13b_chat,
-    *llama2_chat_13b,
-    *chinese_llama_2_13b,
-    *chinese_alpaca_2_13b,
+    # *chatglm2_6b,
+    # *qwen_chat_7b,
+    # *llama2_chat_7b,
+    # *internlm_chat_7b,
+    # *baichuan_13b_chat,
+    *baichuan2_13b_chat,
+    # *llama2_chat_13b,
+    # *chinese_llama_2_13b,
+    # *chinese_alpaca_2_13b,
+    *llama2_chinese_13b_chat,
     #*xverse_13b,
     #*chatgpt,
 ]
 
 for model in models:
+    model['max_out_len'] = 16
     model['path'] = model['path'].replace('/mnt/mfs/opsgpt/','/gpudata/home/cbh/opsgpt/')
     model['tokenizer_path'] = model['tokenizer_path'].replace('/mnt/mfs/opsgpt/', '/gpudata/home/cbh/opsgpt/')
-    if '13' in model['path']:
-        model['run_cfg'] = dict(num_gpus=4, num_procs=1)
-    else:
-        model['run_cfg'] = dict(num_gpus=4, num_procs=8)
-    pass
+    if 'GPT' not in model['abbr']:
+        model['run_cfg'] = dict(num_gpus=2, num_procs=1) if '7b' in model['abbr'] or '6b' in model['abbr'] else dict(num_gpus=4, num_procs=1)
+        model['batch_size'] = 16
 
 for dataset in datasets:
     dataset['path'] = dataset['path'].replace('/mnt/mfs/opsgpt/','/gpudata/home/cbh/opsgpt/')
@@ -56,10 +59,9 @@ for dataset in datasets:
 
 infer = dict(
     partitioner=dict(
-        # type=SizePartitioner,
-        # max_task_size=100,
-        # gen_task_coef=1,
-        type=NaivePartitioner
+        type=SizePartitioner,
+        max_task_size=100,
+        gen_task_coef=1,
     ),
     runner=dict(
         type=LocalRunner,
